2f69671409352ebafcf54fc54d146fc0e8b14934
Chris Lucas <chris@hhd.com.au>
2017-06-08 14:14:08 +1000

    all the eth

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function checkSomeSweetEth() {
+  $.ajax( "https://coinmarketcap-nexuist.rhcloud.com/api/eth" )
+  .done(function(data) {
+    var total = data.price.aud;
+    $(".totesTotal").text('$' + parseFloat(total, 10).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,").toString() + " AUD");
+  });
+
+  $.ajax( "http://api.giphy.com/v1/gifs/random?tag=money&rating=pg-13&api_key=dc6zaTOxFJmzC" )
+  .done(function(data) {
+    $(".totesEth").css('backgroundImage',"url('"+data.data.image_url+"')")
+  });
+
+}
+$(document).ready(function () {
+  checkSomeSweetEth();
+  var timer = setTimeout( function() {
+    checkSomeSweetEth();
+  }, 300000)
+});
---
+++ temp.js
@@ -0,0 +1,1 @@
+https://coinmarketcap-nexuist.rhcloud.com/api/eth
