2c3888fdf0c691feeb8cc2949a42f72d7c3f6f69
Chris Lucas <chris@hhd.com.au>
2017-06-08 16:37:31 +1000

    Pos and neg bro

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-
+var lastPrice = 0;
+var updown = true;
 function checkSomeSweetEth() {
   $.ajax( "https://coinmarketcap-nexuist.rhcloud.com/api/eth" )
   .done(function(data) {
     var total = data.price.aud;
+    if(lastPrice < parseFloat(total, 10)) {
+      updown = true;
+    } else {
+      updown = false;
+    }
+    lastPrice = parseFloat(total, 10)
     $(".totesTotal").text('$' + parseFloat(total, 10).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,").toString());
-  });
-  var randTag = Math.floor((Math.random() * 4));
-  var tags = ["money","swag","rich","lambo"]
-  console.log(tags[randTag]);
-  $.ajax( "https://api.giphy.com/v1/gifs/random?tag="+tags[randTag]+"&rating=pg-13&api_key=dc6zaTOxFJmzC" )
-  .done(function(data) {
-    $(".totesEth").css('backgroundImage',"url('"+data.data.image_url+"')")
-  });
+    if(updown){
+      $(".totesTotal").css('color',"#82ff6e")
+    } else {
+      $(".totesTotal").css('color',"#ff9999")
+    }
 
+    var randTag = Math.floor((Math.random() * 4));
+    var tagsGood = ["money","swag","rich","lambo"];
+    var tagsBad = ["cry","sad","poor","homeless"];
+    var tag = updown ? tagsGood[randTag] : tagsBad[randTag];
+    $.ajax( "https://api.giphy.com/v1/gifs/random?tag="+tag+"&rating=pg-13&api_key=dc6zaTOxFJmzC" )
+    .done(function(data) {
+      $(".totesEth").css('backgroundImage',"url('"+data.data.image_url+"')")
+    });
+  });
 }
 $(document).ready(function () {
   checkSomeSweetEth();
