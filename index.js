d1663512be443d7ab02d54eded85ac76478db6a1
Chris Lucas <chris@hhd.com.au>
2017-06-08 15:48:07 +1000

    Removed AUD and centered text

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function checkSomeSweetEth() {
   $.ajax( "https://coinmarketcap-nexuist.rhcloud.com/api/eth" )
   .done(function(data) {
     var total = data.price.aud;
-    $(".totesTotal").text('$' + parseFloat(total, 10).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,").toString() + " AUD");
+    $(".totesTotal").text('$' + parseFloat(total, 10).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,").toString());
   });
 
   $.ajax( "https://api.giphy.com/v1/gifs/random?tag=money&rating=pg-13&api_key=dc6zaTOxFJmzC" )
